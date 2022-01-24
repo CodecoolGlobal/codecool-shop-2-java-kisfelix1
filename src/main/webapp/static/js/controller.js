64f4aa5be365574e250becd3c90576e6bad030e8
Kis Béla <kisfelix1@gmail.com>
2022-01-24 22:07:28 +0100

    Feature - add eventListener to dropdown

--- src/main/webapp/static/js/controller.js
+++ src/main/webapp/static/js/controller.js
@@ -2,7 +2,12 @@ import {getResponse} from "./model.js";
 import {addEventListener} from "./view.js";
 
 function initialize(){
+    addEventListener('#categories', loadFilteredProducts);
+    addEventListener('#suppliers', loadFilteredProducts);
+}
 
+function loadFilteredProducts(e){
+    console.log(e.target.value);
 }
 
 initialize();
