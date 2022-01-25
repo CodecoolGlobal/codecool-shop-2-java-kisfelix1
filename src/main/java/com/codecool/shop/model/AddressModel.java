af055421c99a4ee3f8c2b2dadea38d0e93e0c1b4
Krisztian Toth <tothkrisztian1569@gmail.com>
2022-01-25 16:07:07 +0100

    Fix: add customer name to adress

--- src/main/java/com/codecool/shop/model/AddressModel.java
+++ src/main/java/com/codecool/shop/model/AddressModel.java
@@ -1,13 +1,15 @@
 package com.codecool.shop.model;
 
 public class AddressModel {
+    private String customerName;
     private String country;
     private String county;
     private int zipCode;
     private String city;
     private String streetAddress;
 
-    public AddressModel(String country, String county, int zipCode, String city, String streetAddress) {
+    public AddressModel(String customerName, String country, String county, int zipCode, String city, String streetAddress) {
+        this.customerName = customerName;
         this.country = country;
         this.county = county;
         this.zipCode = zipCode;
@@ -33,5 +35,9 @@ public class AddressModel {
 
     public String getStreetAddress() {
         return streetAddress;
+    }
+
+    public String getCustomerName() {
+        return customerName;
     }
 }
