12de9ee073a71f21151882613b608925538c10b4
Csipke József <csipkej98@gmail.com>
2022-01-25 11:46:58 +0100

    Feature - Cart elements + Cart button and modal added

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,7 +1,9 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.dao.CartDao;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.CartDaoMem;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.service.ProductService;
@@ -25,6 +27,7 @@ public class ProductController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        CartDao cartDataStore = CartDaoMem.getInstance();
         ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
---
+++ src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
@@ -0,0 +1,46 @@
+package com.codecool.shop.dao.implementation;
+
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CartDaoMem implements CartDao {
+
+    private List<Product> data = new ArrayList<>();
+    private static CartDaoMem instance = null;
+
+    /* A private Constructor prevents any other class from instantiating.
+     */
+    private CartDaoMem() {
+    }
+
+    public static CartDaoMem getInstance() {
+        if (instance == null) {
+            instance = new CartDaoMem();
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(Product product) {
+        product.setId(data.size() + 1);
+        data.add(product);
+    }
+
+    @Override
+    public Product find(int id) {
+        return data.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public void remove(int id) {
+        data.remove(find(id));
+    }
+}
