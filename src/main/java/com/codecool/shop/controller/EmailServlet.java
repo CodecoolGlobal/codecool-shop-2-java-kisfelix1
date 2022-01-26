b6d211e34051d59bd894a546ce918fb00c939c6c
Kis Béla <kisfelix1@gmail.com>
2022-01-26 22:32:47 +0100

    Refactor - remove unused imports

--- src/main/java/com/codecool/shop/controller/EmailServlet.java
+++ src/main/java/com/codecool/shop/controller/EmailServlet.java
@@ -1,9 +1,5 @@
 package com.codecool.shop.controller;
 
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.model.Product;
-import com.google.gson.Gson;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,9 +7,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @WebServlet(urlPatterns = {"/api/sendEmail"})
 public class EmailServlet extends HttpServlet {
