8e7bf2b604a50b3bcff3542b14289f03ec286534
Kis Béla <kisfelix1@gmail.com>
2022-01-26 20:24:47 +0100

    Refactor - extract methods to mvc structure

--- src/main/webapp/static/js/model.js
+++ src/main/webapp/static/js/model.js
@@ -1,4 +1,9 @@
-export {getProductsFiltered};
+export {getProductsFiltered, getCart, sendProductToCart};
+
+function sendProductToCart(id){
+    const url = `/api/add_cart?itemId=${id}`;
+    return getResponse(url);
+}
 
 async function getResponse(url) {
     const response = await fetch(url);
@@ -7,5 +12,9 @@ async function getResponse(url) {
 
 async function getProductsFiltered(categoryId, supplierId) {
     return getResponse(`/api/product?categoryId=${categoryId}&supplierId=${supplierId}`);
+}
+
+async function getCart() {
+    return await getResponse("/api/review_cart");
 }
 
