ecc0cf0280a9b2317509380705727b63461b12cb
Kis Béla <kisfelix1@gmail.com>
2022-01-26 13:13:01 +0100

    Refactor - getAllCategories into ProductService

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -33,7 +33,7 @@ public class ProductController extends HttpServlet {
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("category", productService.getProductCategory(1));
         context.setVariable("products", productService.getProductsForCategory(1));
-        context.setVariable("categories", productCategoryDataStore.getAll());
+        context.setVariable("categories", productService.getAllCategories());
         context.setVariable("suppliers", SupplierDaoMem.getInstance().getAll());
         // // Alternative setting of the template context
         // Map<String, Object> params = new HashMap<>();
--- src/main/java/com/codecool/shop/service/ProductService.java
+++ src/main/java/com/codecool/shop/service/ProductService.java
@@ -4,6 +4,7 @@ import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
+import jdk.jfr.Category;
 
 import java.util.List;
 
@@ -28,6 +29,10 @@ public class ProductService{
             var category = productCategoryDao.find(categoryId);
             return productDao.getBy(category);
         }
+    }
+
+    public List<ProductCategory> getAllCategories(){
+        return productCategoryDao.getAll();
     }
 
 
