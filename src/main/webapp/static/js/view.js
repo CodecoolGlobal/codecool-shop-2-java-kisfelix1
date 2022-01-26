889c7a432bd830ec4315ad327b89fb1634eb94bd
Kis Béla <kisfelix1@gmail.com>
2022-01-26 20:24:53 +0100

    Refactor - extract methods to mvc structure

--- src/main/webapp/static/js/view.js
+++ src/main/webapp/static/js/view.js
@@ -1,4 +1,4 @@
-export {addEventListener,addEventListenerToAll, loadCart, addToCart, loadProducts};
+export {addEventListener,addEventListenerToAll, showProducts, showCart};
 
 function addEventListener(selector, func){
     document.querySelector(selector).addEventListener('click', func);
@@ -8,8 +8,27 @@ function loadContent(selector, content){
     document.querySelector(selector).innerHTML = content;
 }
 
-function loadProducts(products){
+function showProducts(products){
     loadContent('#products',products.map(product => {return buildCard(product)}).join(''));
+}
+
+function showCart(cart){
+    let cartContent = cart.map(prod => {
+        return `<tr>
+            <td class="w-25">
+                <img src="/static/img/product_${prod.id}.jpg"  alt="${prod.name} + '.jpg'" class="img-fluid img-thumbnail" >
+            </td>
+            <td>${prod.name}</td>
+            <td>${prod.defaultPrice} ${prod.defaultCurrency}</td>
+            <td class="qty"><p id=${"amountId" + prod.id} type="text" class="amount form-control" >${prod.amount}</p></td>
+            <td id=${"totalId" + prod.id} data-default-price=${prod.defaultPrice} data-default-currency=${prod.defaultCurrency}>${prod.defaultPrice * prod.amount} ${prod.defaultCurrency}</td>
+            <td data-product-id=${prod.id}>
+                <h2 data-amount="1" class="edit">+</h2>
+                <h2 data-amount="-1" class="edit"> -</h2>
+            </td>
+        </tr>`
+    }).join('');
+    loadContent("#cart-body", cartContent);
 }
 
 function buildCard(product){
@@ -39,29 +58,4 @@ function addEventListenerToAll(selector, func) {
     }
 }
 
-async function loadCart() {
-    let prods = await getResponse("/api/review_cart");
-    let cartContent = prods.map(prod => {
-        return `<tr>
-            <td class="w-25">
-                <img src="/static/img/product_${prod.id}.jpg"  alt="${prod.name} + '.jpg'" class="img-fluid img-thumbnail" >
-            </td>
-            <td>${prod.name}</td>
-            <td>${prod.defaultPrice} ${prod.defaultCurrency}</td>
-            <td class="qty"><p id=${"amountId" + prod.id} type="text" class="amount form-control" >${prod.amount}</p></td>
-            <td id=${"totalId" + prod.id} data-default-price=${prod.defaultPrice} data-default-currency=${prod.defaultCurrency}>${prod.defaultPrice * prod.amount} ${prod.defaultCurrency}</td>
-            <td data-product-id=${prod.id}>
-                <h2 data-amount="1" class="edit">+</h2>
-                <h2 data-amount="-1" class="edit"> -</h2>
-            </td>
-        </tr>`
-    }).join("")
-    loadContent("#cart-body", cartContent)
-}
-
-async function addToCart(e) {
-    const id = e.target.dataset.btnId
-    const url = `/api/add_cart?itemId=${id}`
-    //console.log(getResponse(url))
-}
 
