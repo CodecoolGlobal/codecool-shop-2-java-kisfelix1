c862b7345c8d38ad0cd398afddcd5b966a25f3a8
emis <emisnps@gmail.com>
2022-02-08 14:29:17 +0100

    feat- add title testing

--- src/main/test/java/com/codecool/shop/selenium/BaseTests.java
+++ src/main/test/java/com/codecool/shop/selenium/BaseTests.java
@@ -4,24 +4,36 @@ import com.google.common.annotations.VisibleForTesting;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+
+import static org.testng.Assert.assertEquals;
 
 public class BaseTests {
 
-    private WebDriver driver;
+    private static WebDriver driver;
 
     public void setUp(){
         System.setProperty("webdriver.chrome.driver",
                 "/home/emis/Projects/codecool/oop/week6/codecool-shop-2-java-kisfelix1/src/main/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("http://localhost:8080");
-
-        System.out.println(driver.getTitle());
-        driver.quit();
-
     }
 
     public static void main(String[] args) {
         BaseTests test = new BaseTests();
         test.setUp();
+    }
+
+    @Test
+    public void testGetTitle(){
+        BaseTests test = new BaseTests();
+        test.setUp();
+        String title = "Codecool Shop";
+        assertEquals(driver.getTitle(), title, "Title is not matching");
+        System.out.println(driver.getTitle() + " : driver.getTitle() result.");
+        System.out.println(title + " : our result.");
+        driver.quit();
     }
 }
