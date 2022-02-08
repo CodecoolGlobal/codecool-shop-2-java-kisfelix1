d327c87e21368a629995309523b0dc64005d35aa
emis <emisnps@gmail.com>
2022-02-08 15:44:10 +0100

    Feat - add testSelectedOption() test

--- src/main/test/java/com/codecool/shop/selenium/BaseTests.java
+++ src/main/test/java/com/codecool/shop/selenium/BaseTests.java
@@ -1,14 +1,19 @@
 package com.codecool.shop.selenium;
 
-import com.google.common.annotations.VisibleForTesting;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class BaseTests {
 
@@ -30,5 +35,27 @@ public class BaseTests {
         System.out.println(driver.getTitle() + " : driver.getTitle() result.");
         System.out.println(title + " : our result.");
         driver.quit();
+    }
+
+
+    @Test
+    public void testSelectOption(){
+        setUp();
+        driver.manage().window().maximize();
+        String option = "NFT";
+        findDropDownElement().selectByVisibleText(option);
+        var selectedOptions = getSelectedOption();
+        assertTrue(selectedOptions.contains(option), "Option not selected");
+        driver.quit();
+
+    }
+
+    private List<String> getSelectedOption(){
+        List<WebElement> selectedElements =  findDropDownElement().getAllSelectedOptions();
+        return selectedElements.stream().map(e -> e.getText()).collect(Collectors.toList());
+    }
+
+    private Select findDropDownElement(){
+        return new Select(driver.findElement(By.id("categories")));
     }
 }
