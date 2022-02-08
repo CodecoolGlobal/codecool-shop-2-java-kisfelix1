8bd08c8a0a0eb8d900884ca655953f20d0e56da3
emis <emisnps@gmail.com>
2022-02-08 14:34:04 +0100

    refactor- delete main method

--- src/main/test/java/com/codecool/shop/selenium/BaseTests.java
+++ src/main/test/java/com/codecool/shop/selenium/BaseTests.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-import java.sql.Driver;
+
 
 import static org.testng.Assert.assertEquals;
 
@@ -14,22 +14,17 @@ public class BaseTests {
 
     private static WebDriver driver;
 
-    public void setUp(){
+    private void setUp(){
         System.setProperty("webdriver.chrome.driver",
                 "/home/emis/Projects/codecool/oop/week6/codecool-shop-2-java-kisfelix1/src/main/resources/chromedriver");
         driver = new ChromeDriver();
         driver.get("http://localhost:8080");
     }
 
-    public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-        test.setUp();
-    }
 
     @Test
     public void testGetTitle(){
-        BaseTests test = new BaseTests();
-        test.setUp();
+        setUp();
         String title = "Codecool Shop";
         assertEquals(driver.getTitle(), title, "Title is not matching");
         System.out.println(driver.getTitle() + " : driver.getTitle() result.");
