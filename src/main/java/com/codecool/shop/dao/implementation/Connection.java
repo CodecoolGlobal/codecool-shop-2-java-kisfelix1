3e4ab4ac6824e653cfba3b9a19ba9de0f0c4e155
Kis Béla <kisfelix1@gmail.com>
2022-02-09 16:10:01 +0100

    Feat - database connection method created

---
+++ src/main/java/com/codecool/shop/dao/implementation/Connection.java
@@ -0,0 +1,25 @@
+package com.codecool.shop.dao.implementation;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class Connection {
+    public static DataSource connect() throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        String dbName = System.getenv("DB_NAME");
+        String user = System.getenv("DB_USERNAME");
+        String password = System.getenv("DB_PASSWORD");
+
+        dataSource.setDatabaseName(dbName);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+
+        System.out.println("Trying to connect");
+        dataSource.getConnection().close();
+        System.out.println("Connection ok.");
+
+        return dataSource;
+    }
+}
