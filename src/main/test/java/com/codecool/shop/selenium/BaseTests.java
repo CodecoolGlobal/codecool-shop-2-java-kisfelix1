139d22d9ba90162008691f8beb9e7d82b51e0c31
emis <emisnps@gmail.com>
2022-02-09 10:46:44 +0100

    Feat - add testAddItemToCartAndClickToCheckoutAndFillAllInputField() method

--- src/main/test/java/com/codecool/shop/selenium/BaseTests.java
+++ src/main/test/java/com/codecool/shop/selenium/BaseTests.java
@@ -2,18 +2,14 @@ package com.codecool.shop.selenium;
 
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static org.testng.Assert.assertEquals;
@@ -23,7 +19,10 @@ public class BaseTests {
 
     private static WebDriver driver;
 
-    private void setUp(){
+    private By categories = By.id("categories");
+    private By supplier = By.id("suppliers");
+
+    protected void setUp(){
         System.setProperty("webdriver.chrome.driver",
                 "/home/emis/Projects/codecool/oop/week6/codecool-shop-2-java-kisfelix1/src/main/resources/chromedriver");
         driver = new ChromeDriver();
@@ -47,7 +46,7 @@ public class BaseTests {
         setUp();
         driver.manage().window().maximize();
         String option = "NFT";
-        findDropDownElement().selectByVisibleText(option);
+        findDropDownElement(categories).selectByVisibleText(option);
         var selectedOptions = getSelectedOption();
         assertTrue(selectedOptions.contains(option), "Option not selected");
         driver.quit();
@@ -55,12 +54,12 @@ public class BaseTests {
     }
 
     private List<String> getSelectedOption(){
-        List<WebElement> selectedElements =  findDropDownElement().getAllSelectedOptions();
+        List<WebElement> selectedElements =  findDropDownElement(categories).getAllSelectedOptions();
         return selectedElements.stream().map(e -> e.getText()).collect(Collectors.toList());
     }
 
-    private Select findDropDownElement(){
-        return new Select(driver.findElement(By.id("categories")));
+    private Select findDropDownElement(By dropDownMenu){
+        return new Select(driver.findElement(dropDownMenu));
     }
 
 
@@ -69,7 +68,7 @@ public class BaseTests {
         setUp();
         driver.manage().window().maximize();
         String option = "NFT";
-        findDropDownElement().selectByVisibleText(option);
+        findDropDownElement(categories).selectByVisibleText(option);
         By toxiDogoButton = By.xpath("//button[@data-btn-id='15']");
         driver.findElement(toxiDogoButton).click();
         driver.findElement(By.id("cart")).click();
@@ -79,4 +78,39 @@ public class BaseTests {
         assertEquals(text, "Toxic doge art", "Results text incorrect");
         driver.quit();
     }
+
+
+    @Test
+    public void testAddItemToCartAndClickToCheckoutAndFillAllInputField() throws InterruptedException {
+        setUp();
+        driver.manage().window().maximize();
+        String option = "OpenSea";
+        findDropDownElement(supplier).selectByVisibleText(option);
+        By coolBombArt = By.xpath("//button[@data-btn-id='12']");
+        driver.findElement(coolBombArt).click();
+        driver.findElement(By.id("cart")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("cart-checkout")).click();
+
+        By emailInput = By.id("credit-email");
+        By cardNumber = By.id("card-number");
+        By MMYY = By.id("months-year");
+        By cvvCode = By.id("cvv-code");
+        By nameOnTheCard = By.id("name-on-the-card");
+        By paymentButton = By.id("payment-button");
+
+
+        driver.findElement(emailInput).sendKeys("emistest10@gmail.com");
+        driver.findElement(cardNumber).sendKeys("123456789012");
+        driver.findElement(MMYY).sendKeys("729");
+        driver.findElement(cvvCode).sendKeys("123");
+        driver.findElement(nameOnTheCard).sendKeys("Bence Szabó");
+
+        Thread.sleep(2000);
+        driver.findElement(paymentButton).click();
+        driver.quit();
+    }
+
+
+
 }
