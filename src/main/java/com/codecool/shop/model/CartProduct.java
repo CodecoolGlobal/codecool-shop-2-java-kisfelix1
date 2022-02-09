24d1eee8392f701a43bd9d6aec7e884d1c672337
Kis Béla <kisfelix1@gmail.com>
2022-02-09 17:31:11 +0100

    Fix - CartProduct constructor to match Product imagePath new required parameter

--- src/main/java/com/codecool/shop/model/CartProduct.java
+++ src/main/java/com/codecool/shop/model/CartProduct.java
@@ -5,7 +5,7 @@ public class CartProduct extends Product {
     private int amount;
 
     public CartProduct(Product product, int amount) {
-        super(product.name, product.getDefaultPrice(), product.getDefaultCurrency().toString(), product.description, product.getProductCategory(), product.getSupplier());
+        super(product.name, product.getDefaultPrice(), product.getDefaultCurrency().toString(), product.description,product.getImagePath(), product.getProductCategory(), product.getSupplier());
         this.amount = amount;
         this.id = product.getId();
     }
